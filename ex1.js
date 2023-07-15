a16108f1756ea1dd17425541dcb3ce23743f85c5
Admir-Designer-Programador <134459063+Admir-Designer-Programador@users.noreply.github.com>
2023-07-14 21:55:52 -0300

    Exercicios java Script Laço de Repetição

---
+++ ex1.js
@@ -0,0 +1,59 @@
+// // Lista prof e verificar posicao na Lista
+// Se o numero for par; escreva;
+
+
+// let professores = ["Alana", "Alexia", "Jessica","Odirlei", "Possarle","Thiago","Samanta", "Priscila","Camila",]
+
+// for(let indice = 0; indice < professores.length;indice ++){
+//     console.log("O indice atual é:" + indice)
+// }  
+
+// laço contador (FOR)- Utilizado um contatdor, é usado para repetir um numero exato de vezes
+
+// for(let contador = 0; contador < 3; contador++){
+//     console.log("Boa Noite")
+// }
+
+
+// Laço condicional
+
+// let resposta = prompt("Voce deseja ler uma Boa Noite?")
+
+// while (resposta == "Sim"){
+//     console.log("Boa Noite")
+
+//     resposta = prompt("Voce deseja continua a receber Boa Noite?")
+//     if (resposta == "não"){
+//         console.log("Então não mr procure mais")
+//     }
+// }
+
+// let prompt = require("prompt-sync")()
+
+// let resposta = prompt("Voce deseja ler uma Boa Noite?")
+
+// do {console.log("To nem ai, Boa Noite")
+
+// } while (resposta == ("sim");
+
+
+// Array
+
+let prompt = require("prompt-sync")()
+
+let listaDeNumeros = [1, 2, 5, 7, 16]
+
+console.log(listaDeNumeros[4])
+
+for(let indice = 0; indice < 5; indice++){
+    console.log( "O item de indice $ {indice} é {listaDenumeros[indice]")
+}
+
+//console.log(listaDeNumeros)
+// [Array]
+
+
+
+
+
+
